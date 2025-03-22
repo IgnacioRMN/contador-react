59f2bb7ffb0a70e41e1547be62226098e6bd86f6
Ignacio Ramon <joseignacioramon24@gmail.com>
2025-03-22 12:54:30 -0300

    agregamos bootstrap al proyecto y estilizamos el componente encabezado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
+import Encabezado from "./components/Encabezado";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  
-
   return (
-    <div></div>
-  )
+    <main className="container my-5">
+      <Encabezado />
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Encabezado.jsx
@@ -0,0 +1,19 @@
+const Encabezado = () => {
+  return (
+    <section>
+      <h1 className="display-4 text-center">Primer Proyecto de React</h1>
+      <p className="mt-5">
+        React.js es una de las librerías más populares de JavaScript para el
+        desarrollo de aplicaciones móviles y web. Creada por Facebook, React
+        contiene una colección de fragmentos de código JavaScript reutilizables
+        utilizados para crear interfaces de usuario (UI) llamadas componentes.
+        Es importante señalar que ReactJS no es un framework de JavaScript. Esto
+        porque sólo es responsable de renderizar los componentes de la capa de
+        vista de una aplicación. React es una alternativa a frameworks como
+        Angular y Vue, que permiten crear funciones complejas.
+      </p>
+    </section>
+  );
+};
+
+export default Encabezado;

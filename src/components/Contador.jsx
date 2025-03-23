663ccac959fe6ecf15d3120760086bbdcaf5d392
Ignacio Ramon <joseignacioramon24@gmail.com>
2025-03-23 20:36:59 -0300

    agregamos algunas funciones como por ejemplo la suma

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,12 +1,16 @@
 import { Button } from "react-bootstrap";
 
 
-const Contador = (props) => {
+const Contador = ({infoComision}) => {
+    let numero = 1;
+    const sumar = () => {
+        numero++
+    }
     return (
         <section className="text-center">
-            <h2 >Contador: {props.infoComision}</h2>
-            <p className="lead">1</p>
-            <Button className="me-2">+</Button>
+            <h2 >Contador: {infoComision}</h2>
+            <p className="lead">{numero}</p>
+            <Button className="me-2" onClick={sumar}>+</Button>
             <Button variant="danger">-</Button>
         </section>
     );

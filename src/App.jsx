98af358252888b10d7a6419fb153c2ba7757a7da
Ignacio Ramon <joseignacioramon24@gmail.com>
2025-03-23 17:27:52 -0300

    agregamos el maquetado del componente contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Encabezado from "./components/Encabezado";
 import "bootstrap/dist/css/bootstrap.min.css";
 import InformacionComponente from "./components/InformacionComponente";
+import Contador from "./components/Contador";
 
 function App() {
   return (
     <main className="container my-5">
       <Encabezado />
       <InformacionComponente />
+      <Contador/>
     </main>
   );
 }

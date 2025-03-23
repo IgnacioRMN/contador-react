23b92d6470b5846481c0adbf0f6952e859c17200
Ignacio Ramon <joseignacioramon24@gmail.com>
2025-03-23 17:01:34 -0300

    agregamos el componente InformacionMaquetado con maquetado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Encabezado from "./components/Encabezado";
 import "bootstrap/dist/css/bootstrap.min.css";
+import InformacionComponente from "./components/InformacionComponente";
 
 function App() {
   return (
     <main className="container my-5">
       <Encabezado />
+      <InformacionComponente />
     </main>
   );
 }
---
+++ src/components/InformacionComponente.jsx
@@ -0,0 +1,22 @@
+const InformacionComponente = () => {
+  return (
+    <article>
+      <h2>Componentes</h2>
+      <p>
+        Un componente en React.js, es básicamente una pieza de código
+        encapsulada que combina tanto la lógica de negocio como la interfaz de
+        usuario en una sola unidad reutilizable. La esencia fundamental detrás
+        de los componentes es descomponer una aplicación en partes más pequeñas
+        y manejables, simplificando así la construcción y el mantenimiento del
+        código. Al fusionar la lógica de negocio y la interfaz de usuario en un
+        único componente, se genera una entidad funcional y autónoma que puede
+        emplearse fácilmente en distintas secciones de la aplicación o incluso
+        en aplicaciones completamente diferentes. Este enfoque modular facilita
+        la reutilización del código y contribuye a una arquitectura más
+        escalable y mantenible.
+      </p>
+    </article>
+  );
+};
+
+export default InformacionComponente;

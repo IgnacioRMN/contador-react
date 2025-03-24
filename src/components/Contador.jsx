8daffa7f0f5111bcd253f16020ba9625d3ce3c8e
Ignacio Ramon <joseignacioramon24@gmail.com>
2025-03-23 21:09:58 -0300

    agregamos la funcion para restar

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
 import { Button } from "react-bootstrap";
 
+const Contador = ({ infoComision }) => {
+  const [numero, setNumero] = useState(1);
 
-const Contador = ({infoComision}) => {
-    const [numero, setNumero] = useState(1)
-    const sumar = () => {
-        setNumero(numero + 1)
-    }
-    return (
-        <section className="text-center">
-            <h2 >Contador: {infoComision}</h2>
-            <p className="lead">{numero}</p>
-            <Button className="me-2" onClick={sumar}>+</Button>
-            <Button variant="danger">-</Button>
-        </section>
-    );
+  return (
+    <section className="text-center">
+      <h2>Contador: {infoComision}</h2>
+      <p className="lead">{numero}</p>
+      <Button className="me-2" onClick={() => setNumero(numero + 1)}>
+        +
+      </Button>
+      <Button variant="danger" onClick={() => setNumero(numero - 1)}>
+        -
+      </Button>
+    </section>
+  );
 };
 
 export default Contador;
